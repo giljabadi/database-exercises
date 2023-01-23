08f3c372fc95193160254459a9fd3ff1db08ea63
Gil Jacob Abadi <giljabadi@gmail.com>
2023-01-23 11:49:49 -0600

    initialize

---
+++ albums_migrations.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+
+CREATE TABLE albums (
+    column1_id INT UNSIGNED,
+    column2_artist VARCHAR(50),
+    column3_name VARCHAR(50),
+    column4_release_date INT,
+    column5_sales FLOAT,
+    column6_genre VARCHAR(50)
+);
